e03850a632306651f8e3cf1200916c1d64b0f4a8
Shawn Rider <shawn@shawnrider.com>
2018-01-05 08:55:35 -0800

    Fixed typo.
    Change-Id: Iac97d9105b3bc0f2334b32283618f89e29fbf118

--- detect_faces.py
+++ detect_faces.py
@@ -28,6 +28,6 @@ for (x,y,w,h) in faces:
 
 cv2.imwrite('result.jpg', image)
 
-file = open('faces.json')
+file = open('faces.json', 'w')
 face_data = { 'num': len(faces) }
 json.dump(face_data, file)
