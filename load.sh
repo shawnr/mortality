450cf16a2a892b6706e881941db88ad86ee0b366
Shawn Rider <shawn@shawnrider.com>
2018-01-05 17:10:37 -0800

    Enhancements for boot script.
    Change-Id: Ib074d041129b4a4057547079a004e58c0ddbef7d

--- load.sh
+++ load.sh
@@ -10,9 +10,17 @@ echo "loading mortality viewer"
 cd /home/pi/.virtualenvs/cv
 source bin/activate
 
+echo "cv environment activated"
+
 cd /home/pi/mortality
 python -m http.server &
 
+echo "web server starting"
+
 sleep 5
+
+echo "chromium kiosk starting"
+date
+echo "|---------------------------------|"
 
 chromium-browser --kiosk http://localhost:8000
